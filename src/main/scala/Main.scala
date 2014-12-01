0512df9c742174f1be2173f71f95bc0c693f9dd7
KyleMao <maozeximzx@gmail.com>
2014-11-30 21:12:49 -0500

    First version of demo interface finished, ON A PLANE~

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,5 @@
+import scala.util.matching.Regex
+
 import graph.Graph
 import monitor.Monitor
 import types.DistType
@@ -37,6 +39,12 @@ object Main  {
     m - Go to the main menu
     q - Quit"""
 
+   val userDistOps = """User purchase distribution:
+    number(3-9) - cluster the users n groups and show the group histogram
+    p - View plot of distribution of number of purchases per user
+    m - Go to the main menu
+    q - Quit"""
+
   def main(args: Array[String]): Unit = {
     println(initMsg)
     var ok = true
@@ -47,7 +55,8 @@ object Main  {
       val op = readLine()
       op match {
         case "1" => ok = monitor()
-        //case "2" => ok = prodDist()
+        case "2" => ok = dist(DistType.ProdPur)
+        case "3" => ok = dist(DistType.UserPur)
         case "4" => ok = history()
         case "q" => ok = false
         case _ => println("Unknown option!")
@@ -101,22 +110,27 @@ object Main  {
     ok
   }
 
-  def prodDist() = {
+  def dist(t: DistType.Value) = {
     var ok = true
     var pok = true
+    val intPattern = new Regex("""(\d+)""")
     val gh = new Graph
 
     while (pok) {
-      println(prodDistOps)
+      t match {
+        case DistType.ProdPur => println(prodDistOps)
+        case DistType.UserPur => println(userDistOps)
+      }
       print(prompt)
       val op = readLine()
       op match {
-        case "p" => gh.plotDist(DistType.ProdPur)
+        case "p" => gh.plotDist(t)
         case "m" => pok = false
         case "q" => {
           ok = false
           pok = false
         }
+        case intPattern(op) => gh.drawClusterHist(t, Integer.valueOf(op))
         case _ => println("Unknown option!")
       }
     }
